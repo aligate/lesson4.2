9b73f7c29c542fb32b62e79dedf838b4d929e1bc
aligate <aligate26@gmail.com>
2017-03-23 13:35:42 +0100

    Update view2.php

--- view2.php
+++ view2.php
@@ -2,12 +2,23 @@
   <head>
     <meta charset="utf-8">
     <title>Редактирование</title>
+    
   </head>
   <body>
-	<h2>Редактирование заданий</h2>
+
+    <h2>Редактирование заданий</h2>
 
    <form action = "tasks.php" method = 'POST'>
-		<input type ='text' name='description' size = '50' value ='<?= htmlspecialchars($array['description']); ?>' >
+		<b>Текст:</b> <input type ='text' name='description' size = '50' value ='<?= htmlspecialchars($array['description']); ?>' > <b>Статус:</b>
+		<select name="is_done">
+		<?php if( !$array['is_done'] ): ?>
+            <option value="<?= $array['is_done']; ?>"><span style='color: orange;'>В процессе</span></option>
+            <option value="1">Выполнено</option>
+			<?php else: ?>
+			<option value="<?= $array['is_done']; ?>"><span style='color: green;'>Выполнено</span></option>
+            <option value="0">В процессе</option>
+		<?php endif; ?>
+           </select>
 		<input type ='hidden' name='id' value ='<?= $array['id']; ?>' >
 		<input type="submit" name="change" value="Изменить" />
 	</form>
@@ -15,3 +26,4 @@
 	<p style ="color: blue; font-size:20px;"><a href="?action=cancel">Или вернуться к списку заданий</p>
   </body>
 </html>
+
